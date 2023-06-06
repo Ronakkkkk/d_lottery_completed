2e6ab097afd916db3b79130d57b50dba82a1bf26
Ronak <ronak01.raj@gmail.com>
2023-06-07 00:52:16 +0530

    final hero

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -7,18 +7,24 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-// We need to import the helper functions from the contract that we copy/pasted.
 import { Base64 } from "./libraries/Base64.sol";
 
 contract MyEpicNFT is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
-  string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
-  string[] firstWords = ["Fantastic", "Epic", "Great", "Terrible", "Crazy", "Wild", "Spooky", "Dense","Scary","Terrifying", "Goofy", "Charming"];
-  string[] secondWords = ["Cupcake", "Pizza", "Noodles", "Chicken", "Curry", "Sandwich", "Salad", "MoMo", "Biryani", "ChauChau" "Thakali", "Burger", "Pasta"];
-  string[] thirdWords = ["Thor", "Hulk", "Rocket", "Groot", "Gamora", "Nebula", "Drax", "Mantis", "Tony", "Quill", "Rogers", "Wasp", "Strange", "Wanda"];
-event NewEpicNFTMinted(address sender, uint256 tokenId);
+  // We split the SVG at the part where it asks for the background color.
+  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
+  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+  string[] firstWords = ["BLAH", "BLAH", "BLAH"];
+  string[] secondWords = ["BLAH", "BLAH", "BLAH"];
+  string[] thirdWords = ["BLAH", "BLAH", "BLAH"];
+
+  // Get fancy with it! Declare a bunch of colors.
+  string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+
+  event NewEpicNFTMinted(address sender, uint256 tokenId);
 
   constructor() ERC721 ("SquareNFT", "SQUARE") {
     console.log("This is my NFT contract. Woah!");
@@ -42,8 +48,15 @@ event NewEpicNFTMinted(address sender, uint256 tokenId);
     return thirdWords[rand];
   }
 
+  // Same old stuff, pick a random color.
+  function pickRandomColor(uint256 tokenId) public view returns (string memory) {
+    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
+    rand = rand % colors.length;
+    return colors[rand];
+  }
+
   function random(string memory input) internal pure returns (uint256) {
-      return uint256(keccak256(abi.encodePacked(input)));
+    return uint256(keccak256(abi.encodePacked(input)));
   }
 
   function makeAnEpicNFT() public {
@@ -54,18 +67,17 @@ event NewEpicNFTMinted(address sender, uint256 tokenId);
     string memory third = pickRandomThirdWord(newItemId);
     string memory combinedWord = string(abi.encodePacked(first, second, third));
 
-    string memory finalSvg = string(abi.encodePacked(baseSvg, combinedWord, "</text></svg>"));
+    // Add the random color in.
+    string memory randomColor = pickRandomColor(newItemId);
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
 
-    // Get all the JSON metadata in place and base64 encode it.
     string memory json = Base64.encode(
         bytes(
             string(
                 abi.encodePacked(
                     '{"name": "',
-                    // We set the title of our NFT as the generated word.
                     combinedWord,
                     '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-                    // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
                     Base64.encode(bytes(finalSvg)),
                     '"}'
                 )
@@ -73,23 +85,16 @@ event NewEpicNFTMinted(address sender, uint256 tokenId);
         )
     );
 
-    // Just like before, we prepend data:application/json;base64, to our data.
     string memory finalTokenUri = string(
         abi.encodePacked("data:application/json;base64,", json)
     );
 
     console.log("\n--------------------");
-    console.log(   string(
-        abi.encodePacked(
-            "https://nftpreview.0xdev.codes/?code=",
-            finalTokenUri
-        )
-    ));
+    console.log(finalTokenUri);
     console.log("--------------------\n");
 
     _safeMint(msg.sender, newItemId);
-    
-    // Update your URI!!!
+  
     _setTokenURI(newItemId, finalTokenUri);
   
     _tokenIds.increment();
@@ -97,7 +102,3 @@ event NewEpicNFTMinted(address sender, uint256 tokenId);
     emit NewEpicNFTMinted(msg.sender, newItemId);
   }
 }
-
-
-
-
