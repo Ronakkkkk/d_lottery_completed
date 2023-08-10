8e73962660acf1332f4f58343bc1b8cd13a079bd
Ronak <ronak01.raj@gmail.com>
2023-08-10 23:22:20 +0530

    hh

--- scripts/run.js
+++ scripts/run.js
@@ -1,31 +1,57 @@
 
+const main = async () => {
+  try {
+    const lottryContractFactory = await hre.ethers.getContractFactory("DecentralizedLottery");
+  const minimumBet = ethers.utils.parseEther("0");
+  const durationInBlocks = 10;
+  
+  const lotterycontract = await lottryContractFactory.deploy(minimumBet, durationInBlocks, {
+    value: ethers.utils.parseEther("0.1")
+  });
 
-  const main = async () => {
-    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
-    const nftContract = await nftContractFactory.deploy();
-    await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address);
+ await lotterycontract.deployed();
   
-    // Call the function.
-    let txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-  
-    // Mint another NFT for fun.
-    txn = await nftContract.makeAnEpicNFT()
-    // Wait for it to be mined.
-    await txn.wait()
-  
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  console.log("Contract addy:", lotterycontract.address);
+
+  contractBalance = await ethers.provider.getBalance(
+    lotterycontract.address
+  );
+  console.log(
+    "Contract balance:",
+    ethers.utils.formatEther(contractBalance)
+  );
+
+  const enterTxn = await lotterycontract.enterLottery();
+  await enterTxn.wait();
+
+  const allPlayers = await lotterycontract.getAllPlayers();
+  console.log("All Players:");
+  for (let i = 0; i < allPlayers.length; i++) {
+    console.log(`Player ${i + 1}:`, allPlayers[i]);
+  }
+
+  const pickWinnerTxn = await lotterycontract.pickWinner();
+  await pickWinnerTxn.wait();
+
+   contractBalance = await ethers.provider.getBalance(lotterycontract.address);
+  console.log(
+    "Done, Contract balance:",
+    ethers.utils.formatEther(contractBalance)
+  );
+    
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
