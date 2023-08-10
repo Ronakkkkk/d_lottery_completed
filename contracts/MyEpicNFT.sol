c683581a17c102e4d78245938fafb2d1cc26e5ae
Ronak <ronak01.raj@gmail.com>
2023-08-11 00:22:09 +0530

    hh

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,104 +1,58 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.18;
-
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+pragma solidity ^0.8.17;
 import "hardhat/console.sol";
+contract DecentralizedLottery {
+    address public manager;
+    address[] public players;
+    uint256 public minimumbet;
+    uint256 public lotteryEnd;
+    uint256 private seed;
+    event WinnerSelected(address winner);
+    constructor(uint256 _minimumbet,uint256 _durationInBlocks) payable {
+        manager=msg.sender;
+        minimumbet=_minimumbet;
+        lotteryEnd=block.number+_durationInBlocks;
+        seed=(block.timestamp+block.difficulty)%100;
+    }
 
-import { Base64 } from "./libraries/Base64.sol";
+    function enterLottery()public payable{
+require(msg.value>=minimumbet, "Insufficinet Bet ammount");
+require(block.number<lotteryEnd,"Lottery has ended");
 
-contract MyEpicNFT is ERC721URIStorage {
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIds;
+players.push(msg.sender);
+console.log("%s entered", msg.sender);
+        
+    }
 
-  // We split the SVG at the part where it asks for the background color.
-  string svgPartOne = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='";
-  string svgPartTwo = "'/><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+    function getAllPlayers() public view returns (address[] memory) {
+        return players;
+    }
 
-  string[] firstWords = ["Fantatstic", "Great", "Maginficent", "Inredible","Scary", "Creepy", "Euphoric", "Interesting", "Energetic", "Curious", "Happy"];
-  string[] secondWords = ["MOMO", "Pitza", "Burger", "Biryani", "Chole-Bhature", "Tandoori", "Stake", "Salad", "Sandwich","Milkshake", "Coffee", "chocolate"];
-  string[] thirdWords = ["Hulk", "Thor", "Tony", "Steve", "Grooot", "Rocket", "Gamora", "Nebulla", "Quill", "Ant-man", "Black-Panther", "Drax"];
+function pickWinner() public restricted {
+    require(block.number > lotteryEnd, "Lottery is still ongoing");
 
-  // Get fancy with it! Declare a bunch of colors.
-  string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
-
-  event NewEpicNFTMinted(address sender, uint256 tokenId);
-
-  constructor() ERC721 ("SquareNFT", "SQUARE") {
-    console.log("This is my NFT contract. Woah!");
-  }
-
-  function pickRandomFirstWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
-    rand = rand % firstWords.length;
-    return firstWords[rand];
-  }
-
-  function pickRandomSecondWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId))));
-    rand = rand % secondWords.length;
-    return secondWords[rand];
-  }
-
-  function pickRandomThirdWord(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId))));
-    rand = rand % thirdWords.length;
-    return thirdWords[rand];
-  }
-
-  // Same old stuff, pick a random color.
-  function pickRandomColor(uint256 tokenId) public view returns (string memory) {
-    uint256 rand = random(string(abi.encodePacked("COLOR", Strings.toString(tokenId))));
-    rand = rand % colors.length;
-    return colors[rand];
-  }
-
-  function random(string memory input) internal pure returns (uint256) {
-    return uint256(keccak256(abi.encodePacked(input)));
-  }
-
-  function makeAnEpicNFT() public {
-    uint256 newItemId = _tokenIds.current();
-
-    string memory first = pickRandomFirstWord(newItemId);
-    string memory second = pickRandomSecondWord(newItemId);
-    string memory third = pickRandomThirdWord(newItemId);
-    string memory combinedWord = string(abi.encodePacked(first, second, third));
-
-    // Add the random color in.
-    string memory randomColor = pickRandomColor(newItemId);
-    string memory finalSvg = string(abi.encodePacked(svgPartOne, randomColor, svgPartTwo, combinedWord, "</text></svg>"));
-
-    string memory json = Base64.encode(
-        bytes(
-            string(
-                abi.encodePacked(
-                    '{"name": "',
-                    combinedWord,
-                    '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
-                    Base64.encode(bytes(finalSvg)),
-                    '"}'
-                )
-            )
-        )
-    );
-
-    string memory finalTokenUri = string(
-        abi.encodePacked("data:application/json;base64,", json)
-    );
-
-    console.log("\n--------------------");
-    console.log(finalTokenUri);
-    console.log("--------------------\n");
-
-    _safeMint(msg.sender, newItemId);
+    seed = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    uint256 winnerIndex = seed % players.length;
+    console.log(players.length);
+    console.log(winnerIndex);
   
-    _setTokenURI(newItemId, "ipfs://QmYQB8E52YKDr6Zex7p13vDPnUpequa7pAA2wDwQmFikT9");
-  
-    _tokenIds.increment();
-    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
-    emit NewEpicNFTMinted(msg.sender, newItemId);
-  }
+    address winner = players[winnerIndex];
+    console.log("%s is the winner", winner);
+
+    uint256 balance = address(this).balance;
+    payable(winner).transfer(balance);
+    console.log("funds transferred");
+
+    emit WinnerSelected(winner);
+}
+
+modifier restricted() {
+    require(msg.sender == manager, "Only the manager can perform this action");
+    _;
+}
+
+function setManager(address newManager) public restricted {
+    manager = newManager;
+}
 }
