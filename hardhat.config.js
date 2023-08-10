6198d71fdf0b30727931c96632af62de75ea5c26
Ronak <ronak01.raj@gmail.com>
2023-08-11 00:22:01 +0530

    final

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,9 @@ require("dotenv").config();
 module.exports = {
   solidity: '0.8.18',
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",
+    goerli: {
+      url: process.env.STAGING_QUICKNODE_KEY,
+      accounts: [process.env.PRIVATE_KEY]
     },
   },
 };
